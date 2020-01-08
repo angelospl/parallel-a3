92c057f2b006efa8517e1ae14669035e648675f1
angelospl <angelospl48@gmail.com>
2020-01-09 00:56:50 +0200

    Add array lock. Sync and fetch version

--- z2/array_lock.c
+++ z2/array_lock.c
@@ -1,29 +1,43 @@
 #include "lock.h"
 #include "../common/alloc.h"
 
+
 struct lock_struct {
-	/* Delete this in your implementation, just a placeholder. */
-	int dummy;
+        int size;
+        int flag[64];
+				int tail;
 };
+
+__thread unsigned long int myslot;      //thread local integer variable
+
 
 lock_t *lock_init(int nthreads)
 {
-	lock_t *lock;
+        lock_t *lock;
+        int i;
+        XMALLOC(lock, 1);
 
-	XMALLOC(lock, 1);
-	/* other initializations here. */
-	return lock;
+        lock->size = nthreads;
+        for (i=1;i<lock->size;i++) lock->flag[i] = 0;
+        lock->flag[0]=1;
+        lock->tail = 0;
+
+        return lock;
 }
 
 void lock_free(lock_t *lock)
 {
-	XFREE(lock);
+        XFREE(lock);
 }
 
 void lock_acquire(lock_t *lock)
 {
+        myslot = __sync_fetch_and_add(&lock->tail,1);
+        while(!(lock->flag[myslot%lock->size])){};
 }
 
 void lock_release(lock_t *lock)
 {
+        lock->flag[myslot%lock->size] = 0;
+        lock->flag[(myslot+1)%lock->size] = 1;
 }
