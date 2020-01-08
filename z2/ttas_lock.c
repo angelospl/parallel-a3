aaec8fe02d501f1716a73aaf724dca434809a3bc
angelospl <angelospl48@gmail.com>
2020-01-08 22:52:57 +0200

    Add ttas lock

--- z2/ttas_lock.c
+++ z2/ttas_lock.c
@@ -1,29 +1,42 @@
 #include "lock.h"
 #include "../common/alloc.h"
 
+typedef enum{
+        UNLOCKED=0,
+        LOCKED
+}lock_state_t;
+
 struct lock_struct {
-	/* Delete this in your implementation, just a placeholder. */
-	int dummy;
+        lock_state_t state;
 };
 
 lock_t *lock_init(int nthreads)
 {
-	lock_t *lock;
+        lock_t *lock;
 
-	XMALLOC(lock, 1);
-	/* other initializations here. */
-	return lock;
+        XMALLOC(lock, 1);
+        /* other initializations here. */
+        lock->state = UNLOCKED;
+        return lock;
 }
 
 void lock_free(lock_t *lock)
 {
-	XFREE(lock);
+        XFREE(lock);
 }
 
 void lock_acquire(lock_t *lock)
 {
+        lock_t *ttas_lock = lock;
+        while(1){
+                while(ttas_lock->state == LOCKED) {};
+                if(__sync_lock_test_and_set(&ttas_lock->state, LOCKED) != LOCKED)
+                        return;
+        }
 }
 
 void lock_release(lock_t *lock)
 {
+        lock_t *ttas_lock = lock;
+        __sync_lock_release(&ttas_lock->state);
 }
