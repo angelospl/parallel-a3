8bda8b7db07e6c27cbe46cdbd3eb8eaf682fdef3
angelospl <angelospl48@gmail.com>
2020-01-08 22:32:11 +0200

    Add pthread_locks

--- z2/pthread_lock.c
+++ z2/pthread_lock.c
@@ -1,9 +1,9 @@
 #include "lock.h"
 #include "../common/alloc.h"
+#include <pthread.h>
 
 struct lock_struct {
-	/* Delete this in your implementation, just a placeholder. */
-	int dummy;
+	pthread_spinlock_t *p_lock;
 };
 
 lock_t *lock_init(int nthreads)
@@ -11,19 +11,22 @@ lock_t *lock_init(int nthreads)
 	lock_t *lock;
 
 	XMALLOC(lock, 1);
-	/* other initializations here. */
+	pthread_spin_init(&(lock->p_lock),PTHREAD_PROCESS_SHARED);
 	return lock;
 }
 
 void lock_free(lock_t *lock)
 {
+	pthread_spin_destroy(&(lock->p_lock));
 	XFREE(lock);
 }
 
 void lock_acquire(lock_t *lock)
 {
+	pthread_spin_lock(&(lock->p_lock));
 }
 
 void lock_release(lock_t *lock)
 {
+	pthread_spin_unlock(&(lock->p_lock));
 }
